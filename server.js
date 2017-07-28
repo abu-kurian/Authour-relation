85be488b00403d994d8332dd2023dbd5f5fd82b8
Anu Kurian <anukurian16@gmail.com>
2017-07-29 05:17:54 +0530

    basic app structure

---
+++ server.js
@@ -0,0 +1,20 @@
+import fetch from "isomorphic-fetch";
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+
+app.set('port', (process.env.PORT || 3001));
+
+// Express only serves static assets in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+
+app.get('/hi', (req, res) => {
+  res.json("hi")
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`Find the server at: http://localhost:${app.get('port')}/`); // eslint-disable-line no-console
+});
