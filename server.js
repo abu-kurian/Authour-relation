a31b53ed4dfeb9463ab123fc6126d85fd57f38cb
Abu Kurian <abukurian97@gmail.com>
2017-08-02 23:46:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 }
 
-var driver = neo4j.driver('bolt://localhost',neo4j.auth.basic('neo4j','anuabuann'));
+var driver = neo4j.driver('bolt://localhost',neo4j.auth.basic('neo4j','kateisdog'));
 var session = driver.session();
 
 function createMyJson(data){
