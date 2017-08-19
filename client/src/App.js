818a0d6b1b8f5b93c50fa9de9eb76ed24c4d5a7f
abu-kurian <abukurian97@gmail.com>
2017-08-20 01:03:27 +0530

    styling

--- client/src/App.js
+++ client/src/App.js
@@ -56,7 +56,9 @@ class App extends Component {
     super(props);
     this.__onNameChange = this.__onNameChange.bind(this);
     this.__getConnections = this.__getConnections.bind(this);
+    this.__getAuthourData = this.__getAuthourData.bind(this);
     this.__closeModal = this.__closeModal.bind(this);
+    this.__display = this.__display.bind(this);
 
     this.state = {
       graph: {
@@ -64,7 +66,9 @@ class App extends Component {
         edges:[]
       },
       name: '',
-      modelOpen: false
+      modelOpen: false,
+      author: [],
+      modalType: ''
     };
   }
 
@@ -75,7 +79,6 @@ class App extends Component {
 
   __closeModal(){
     this.setState({modalOpen:false});
-    this.setState({name:''})
   }
 
   __getConnections(){
@@ -87,27 +90,109 @@ class App extends Component {
       };
       this.setState({graph: graphLocal});
       this.setState({modalOpen: true});
+      this.setState({modalType: "GraphModal"});
+      //var check =0;
     })
     .catch(()=>{console.log("error");})
   }
+
+  __getAuthourData(){
+    PersonAPI.getConnections(this.state.name)
+    .then((result)=>{
+      //var country = result.country;
+      //var thesis = result.thesis;
+      //var institute = result.institute;
+      console.log(result.country);
+      let authourData = {
+        country: result.country,
+        thesis: result.thesis,
+        institute: result.insitute
+      };
+      this.setState({author: authourData});
+      this.setState({modalOpen: true});
+      //var check=1;
+      this.setState({modalType: "DataField"});
+    })
+    .catch(()=>{console.log("error");})
+  }
+
+  __display() {
+    console.log("modal type",this.state.modalType);
+    if(this.state.modalType==="GraphModal"){
+      return(
+      <div>
+        <div className="LegendBorder">
+          <div className="LEGEND">
+            <span><u>Legend</u></span>
+          </div>
+          <div className="AuthourLegend">
+            <span>Referenced author</span>
+          </div>
+          <div className="Level1Legend">
+            <span>Level 1</span>
+          </div>
+          <div className="Level2Legend">
+            <span>Level 2</span>
+          </div>
+        </div>
+        <Graph graph={this.state.graph} options={options} events={events} />
+      </div>);
+    }
+    if(this.state.modalType==="DataField"){
+      return(
+        <div>
+          <h1>Author Information</h1>
+          <p>
+          <span><b>Name: </b></span>
+          <span>{this.state.name}</span>
+          </p>
+          <p>
+          <span><b>Thesis: </b></span>
+          <span>{this.state.author.thesis}</span>
+          </p>
+          <p>
+          <span><b>Country: </b></span>
+          <span>{this.state.author.country}</span>
+          </p>
+          <p>
+          <span><b>Institute: </b></span>
+          <span>{this.state.author.institute}</span>
+          </p>
+        </div>
+      );
+    }
+    return <h1>ji</h1>
+  }
+
 
 
   render() {
     return (
       <div className="App">
-        <h1>Computer Science Genealogy Tree</h1>
+        <h1>Computer Science Genealogy Graph</h1>
         <input type="text"
               placeholder="Enter Name"
               onChange={this.__onNameChange}
               value={this.state.name}
               />
-        <button className="searchButton" onClick = {this.__getConnections} >search</button>
+        <div>
+          <button className="searchButton" onClick = {this.__getConnections} >plot graph</button>
+          <button className="getDataButton" onClick = {this.__getAuthourData} >Get authour data</button>
+        </div>
+        <div className="rightsReserved">
+            <span>All rights reserved by SciBase</span>
+        </div>
         <Modal
             isOpen={this.state.modalOpen}
             style={customStyle}
             contentLabel="Modal">
-            <button onClick = {this.__closeModal} >Close</button>
-            <Graph graph={this.state.graph} options={options} events={events} />
+            <div>
+              <button onClick = {this.__closeModal} >Close</button>
+              {
+                this.__display()
+            }
+            </div>
+
           </Modal>
       </div>
     );
--- server.js
+++ server.js
@@ -20,8 +20,11 @@ function createMyJson(data){
   let edgeCount=0;
   let nodes = [];
   let edges = [];
+  let thesis = data[6];
+  let country = data[7];
+  let insitute = data[8];
   var newEdge;
-  let newNode = {id:1, label:data[0], color: '#D2EF1A'};
+  let newNode = {id:1, label:data[0], color: '#808000'};
   nodes.push(newNode);
   let data2= JSON.parse(data[2]);
   data2.map(function(connection) {
@@ -68,14 +71,18 @@ function createMyJson(data){
     }
 
   });
-  return {nodes, edges};
+  return {nodes, edges, thesis, country, insitute};
 }
 
 app.get('/person', (req, res) => {
   session
-    .run(`match(n) where n.name='${req.query.name}' RETURN n.name,n.level1,n.level2,n.level3,n.level4,n.level5`)
+    .run(`match(n) where n.name='${req.query.name}' RETURN n.name,n.level1,n.level2,n.level3,n.level4,n.level5,n.thesis,n.country,n.institute`)
     .then(function(result){
+
       const singleRecord = result.records[0];
+      let data = singleRecord._fields;
+      //let data2= JSON.parse(data[1]);
+      //console.log(data[6]);
       res.json(createMyJson(singleRecord._fields));
     })
     .catch(function(err){
